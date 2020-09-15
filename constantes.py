defb39c59b50d025d22c079106e2b4dd8caeaff5
mariaadradosr <mariaadradosr@gmail.com>
2020-09-15 13:07:17 +0200

    new function to get detail added

---
+++ constantes.py
@@ -0,0 +1,54 @@
+diccionario_orange = {
+    'fbb_conv': ['FBB Convergencia',
+                 {
+                     4: 'fbb_conv_doble_capta',
+                     5: 'fbb_conv_x_sell'
+                 }],
+    'fbb_only': ['FBB Stand Alone',
+                 {
+                     0: 'fbb_only'
+                 }],
+    'mov_conv': ['Móvil Convergencia',
+                 {
+                     5: 'mov_conv_1as_lineas',
+                     8: 'mov_conv_2as_lineas',
+                     11: 'mov_conv_doble_capta',
+                     14: 'mov_conv_x_sell',
+                     17: 'ampliaciones'
+                 }],
+    'mov_only': ['Mobile Only',
+                 {
+                     3: 'mov_only_voice_data',
+                     6: 'mov_only_data',
+                     7: 'mov_only_4G',
+                     8: 'mov_only_mi_fijo',
+                 }],
+    'mix_canal': ['Mix canal Osp',
+                  {
+                      3: 'mix_fbb_vtas_exclusivo_ttpp',
+                      5: 'mix_fbb_vtas_exclusivo_franq',
+                      6: 'mix_fbb_vtas_exclusivo_resto',
+                      8: 'mix_fbb_vtas_eshop_tw',
+                      9: 'mix_fbb_vtas_eshop_web_online',
+                      11: 'mix_fbb_vtas_plataformas_inbound',
+                      12: 'mix_fbb_vtas_plataformas_outbound',
+                      16: 'mix_fbb_vtas_plataformas_resto',
+                      17: 'mix_fbb_vtas_no_exclusivo',
+                      18: 'mix_fbb_vtas_resto',
+                      19: 'mix_fbb_vtas_salesforce',
+                      22: 'mix_fbb_op_retencion',
+                      23: 'mix_fbb_op_fidelizacion',
+                      24: 'mix_fbb_op_resto',
+                      28: 'mix_mov_vtas_no_exclusivo_phone_house',
+                      29: 'mix_mov_vtas_no_exclusivo_ggss',
+                      30: 'mix_mov_vtas_no_exclusivo_resto',
+                      32: 'mix_mov_vtas_exclusivo_ttpp',
+                      33: 'mix_mov_vtas_exclusivo_franquicia',
+                      34: 'mix_mov_vtas_exclusivo_resto',
+                      36: 'mix_mov_vtas_plataformas_inbound',
+                      37: 'mix_mov_vtas_plataformas_outbound',
+                      39: 'mix_mov_vtas_eshop_eshop',
+                      40: 'mix_mov_vtas_eshop_tw',
+                      42: 'mix_mov_op_total'
+                  }]
+}
--- main.py
+++ main.py
@@ -1,44 +1,88 @@
+# -*- coding: utf-8 -*-
 import functions
 import pandas as pd
-import datetime 
+import datetime
 import numpy as np
+import constantes
 
 osp_migras_path = './input/migras.xlsx'
 osp_daily_path = './input/orange.xlsx'
 jz_path = './input/jazztel.xlsx'
 output_path = './output/'
 
+print('loading OSP DB')
+xls = pd.ExcelFile(osp_daily_path)
+print('OSP DB loaded')
+
 
 def main():
     print("Generando documento ......\n")
-    fbb_sa = functions.daily(osp_daily_path, sheet = 'FBB Stand Alone', row = 0, name = 'osp_fbb_stand_alone' )
-    fbb_co = functions.daily(osp_daily_path, sheet = 'FBB Convergencia', row = 0, name = 'osp_fbb_convergencia')
-    mov_co = functions.daily(osp_daily_path, sheet = 'Móvil Convergencia', row = 0, name = 'osp_mov_convergencia')
-    mov_on = functions.daily(osp_daily_path, sheet = 'Mobile Only', row = 0, name = 'osp_mov_only')
-    amena_fbb = functions.daily(osp_daily_path, sheet = 'FBB AMENA', row = 10, name = 'amena_fbb')
-    amena_mov = functions.daily(osp_daily_path, sheet = 'Total Móvil', row = 8, name = 'amena_mov')
+    fbb_sa = functions.daily(xls, sheet='FBB Stand Alone',
+                             row=0, name='osp_fbb_stand_alone')
+    fbb_co = functions.daily(
+        xls, sheet='FBB Convergencia', row=0, name='osp_fbb_convergencia')
+    mov_co = functions.daily(
+        xls, sheet='Móvil Convergencia', row=0, name='osp_mov_convergencia')
+    mov_on = functions.daily(xls, sheet='Mobile Only',
+                             row=0, name='osp_mov_only')
+    amena_fbb = functions.daily(
+        xls, sheet='FBB AMENA', row=10, name='amena_fbb')
+    amena_mov = functions.daily(
+        xls, sheet='Total Móvil', row=8, name='amena_mov')
     osp_mig = functions.migras(osp_migras_path)
     jazztel = functions.jazztel(jz_path)
-    orange = pd.concat([fbb_co,fbb_sa,mov_co,mov_on,osp_mig], axis=1, sort=False).fillna(0)
-    orange['orange']=orange.iloc[:,0]+orange.iloc[:,1]+orange.iloc[:,2]+orange.iloc[:,3]+orange.iloc[:,4]
-    amena = pd.concat([amena_fbb, amena_mov], axis = 1, sort = False).fillna(0)
-    amena['amena'] = amena.iloc[:,0]+amena.iloc[:,1]
-    ventas = pd.concat([orange,jazztel,amena], axis = 1, sort = False).fillna(0)
+    orange = pd.concat([fbb_co, fbb_sa, mov_co, mov_on,
+                        osp_mig], axis=1, sort=False).fillna(0)
+    orange['orange'] = orange.iloc[:, 0]+orange.iloc[:, 1] + \
+        orange.iloc[:, 2]+orange.iloc[:, 3]+orange.iloc[:, 4]
+    amena = pd.concat([amena_fbb, amena_mov], axis=1, sort=False).fillna(0)
+    amena['amena'] = amena.iloc[:, 0]+amena.iloc[:, 1]
+    ventas = pd.concat([orange, jazztel, amena], axis=1, sort=False).fillna(0)
     ventas.index = pd.to_datetime(ventas.index)
     ventas.index = ventas.asfreq('d').index
     semanas = ventas.index.to_series().dt.week
     ventas['semana'] = semanas
     ventas.index.name = 'fecha'
-    s = pd.read_csv('./semanas.csv',sep=";")
+    s = pd.read_csv('./semanas.csv', sep=";")
     s['fecha'] = pd.to_datetime(s.fecha)
     s.set_index('fecha', inplace=True)
     idx = pd.date_range(s.index.min(), s.index.max())
     s.index = pd.DatetimeIndex(s.index)
-    s = s.reindex(idx, fill_value = np.nan).ffill().astype('int64')
+    s = s.reindex(idx, fill_value=np.nan).ffill().astype('int64')
     s.index.name = 'fecha'
-    merge=pd.merge(ventas,s, how='left', left_index=True, right_index=True)
-    merge.to_csv(f'{output_path}ventas_{datetime.date.today().strftime("%d%m%y")}.csv', decimal=",",encoding='CP1252')
-    merge.to_csv(f'{output_path}ventas.csv', decimal=",",encoding='CP1252')
-    print(f'Archivo guardado [{output_path}ventas_{datetime.date.today().strftime("%d%m%y")}.csv]')
+    merge = pd.merge(ventas, s, how='left', left_index=True, right_index=True)
+    merge.to_csv(f'{output_path}ventas_{datetime.date.today().strftime("%d%m%y")}.csv',
+                 decimal=",", encoding='CP1252')
+    merge.to_csv(f'{output_path}ventas.csv', decimal=",", encoding='CP1252')
+    print(
+        f'Archivo guardado [{output_path}ventas_{datetime.date.today().strftime("%d%m%y")}.csv]')
+
+    fbb_conv = functions.deepDaily(
+        xls, 'fbb_conv', constantes.diccionario_orange)
+    fbb_only = functions.deepDaily(
+        xls, 'fbb_only', constantes.diccionario_orange)
+    mov_conv = functions.deepDaily(
+        xls, 'mov_conv', constantes.diccionario_orange)
+    mov_only = functions.deepDaily(
+        xls, 'mov_only', constantes.diccionario_orange)
+    mix_canal = functions.deepDaily(
+        xls, 'mix_canal', constantes.diccionario_orange)
+    total_osp = pd.concat([fbb_conv, fbb_only, mov_conv,
+                           mov_only, mix_canal], axis=1, sort=False).fillna(0)
+
+    total_osp.reset_index(inplace=True)
+    total_osp['dia_semana'] = total_osp['index'].dt.weekday
+    total_osp['timedelta'] = total_osp.dia_semana.apply(
+        lambda x: pd.Timedelta(days=x))
+    total_osp['fecha_semana'] = total_osp['index'] - total_osp.timedelta
+
+    total_osp.drop(columns=['dia_semana', 'timedelta'], inplace=True)
+
+    total_osp.to_csv(
+        f'{output_path}total_orange_{datetime.date.today().strftime("%d%m%y")}.csv', decimal=",", encoding='CP1252', index=False)
+    total_osp.to_csv(f'{output_path}total_orange.csv',
+                     decimal=",", encoding='CP1252', index=False)
+
+
 if __name__ == "__main__":
     main()
